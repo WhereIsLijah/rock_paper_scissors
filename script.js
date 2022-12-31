e93f367723834407d96ed59ec90f34cb11961e67
WhereIsLijah <ayomideoduba@gmail.com>
2022-12-31 02:02:43 -0500

    Score updates and Round selection resolved

--- script.js
+++ script.js
@@ -1,68 +1,58 @@
- function getComputerChoice() //Function to get random choice of AI
- {
-    let choices = Array('Rock', 'Paper', 'Scissors');
-    var compChoice = choices[Math.floor(Math.random()*choices.length)];
-    return compChoice;
- }
+let cScore = 0;
+let playScore = 0;
 
- 
- function play(playerSelection, computerSelection) // Function 
- { 
+function aiScore(score) {
+  let result = 0;
+  result += score;
+  return result;
+}
 
-      if ((playerSelection == "Rock") && (computerSelection == "Rock"))
-         {
-            return `Player: ${playerSelection} | AI: ${computerSelection} | Draw! Try Again!`;
-         }
-      else if ((playerSelection == "Scissors") && (computerSelection == "Scissors"))
-         {
-            return `Player: ${playerSelection} | AI: ${computerSelection} | Draw! Try Again!`;
-         }
-      else if ((playerSelection == "Paper") && (computerSelection == "Paper"))
-         {
-            return `Player: ${playerSelection} | AI: ${computerSelection} | Draw! Try Again!`;
-         }
-      else if ((playerSelection == "Rock") && (computerSelection == "Paper"))
-         {
-            return `Player: ${playerSelection} | AI: ${computerSelection} | You Lose! Paper Beats Rock`;
-         }
-      else if ((playerSelection == "Paper") && (computerSelection == "Scissors"))
-         {
-            return `Player: ${playerSelection}| AI: ${computerSelection} | You Lose! Scissors Beats Paper`;
-         }
-      else if ((playerSelection == "Scissors") && (computerSelection == "Rock"))
-         {
-            return `Player: ${playerSelection} | AI: ${computerSelection} | You Lose! Rock Beats Scissors`;
-         }
-      else if ((playerSelection == "Paper") && (computerSelection == "Rock"))
-         {
-            return `Player: ${playerSelection} | AI: ${computerSelection} | You Win! Paper Beats Rock`;
-         }
-      else if ((playerSelection == "Scissors") && (computerSelection == "Paper"))
-         {
-            return `Player: ${playerSelection} | AI: ${computerSelection} | You Win! Scissors Beats Paper`;
-         }
-      else if ((playerSelection == "Rock") && (computerSelection == "Scissors"))
-         {
-            return `Player: ${playerSelection} | AI: ${computerSelection} | You Win! Rock Beats Scissors`;
-         }
-      else {
-            return "Invalid Input";
-         }
+function playerScore(score) {
+  let result = 0;
+  result += score;
+  return result;
+}
+
+function getaiChoice() {  //This Function gets a random choice of the AI
+  let choices = Array("rock", "paper", "scissors");
+  let aiChoice = choices[Math.floor(Math.random() * choices.length)];
+  return aiChoice;
+}
+
+function play(playerSelection, aiSelection) {
+   if (playerSelection == aiSelection) {
+    return `You Played: ${playerSelection}| AI played: ${aiSelection}| Draw!`;
+   } 
+   else if ((playerSelection == "rock" && aiSelection == "paper") ||
+            (playerSelection == "paper" && aiSelection == "scissors") ||
+            (playerSelection == "scissors" && aiSelection == "rock")){
+                   cScore++;
+                   aiScore(cScore);
+             return `You Played: ${playerSelection} | AI played: ${aiSelection} | You Lose!`;
+            }
+   else if ((playerSelection == "paper" && aiSelection == "rock") ||
+            (playerSelection == "scissors" && aiSelection == "paper") ||
+            (playerSelection == "rock" && aiSelection == "scissors")){
+                   playScore++;
+                   aiScore(playScore);
+             return `You Played: ${playerSelection} | AI played: ${aiSelection} | You Win!`;
+  } else {
+    return "Invalid Input";
+  }
+}
+
+function game() {
+  let res = "";
+  let finalScore;
+
+  num = parseInt(prompt("How Many Rounds do you want to Play?"));
+  
+   for (let i = 0; i < num; i++) {
+      playerSelection = prompt("Rock, Paper or Scissors?");
+      res += play(playerSelection, getaiChoice()) + "<br/>";
+      document.getElementById("test").innerHTML = res;
    }
-
- function game()
- {
-      //num = parseInt(prompt("How Many Rounds do you want to Play?"));
-      //let playerScore = 0;
-      let score="";
-
-      for(let i = 0; i < 3; i++)
-      {
-         playerSelection = prompt("Rock, Paper or Scissors?");
-         score += play(playerSelection, getComputerChoice()) + "<br/>";
-         document.getElementById("test").innerHTML = score;
-      } // Add scores and make user enter amount of rounds
-      
- }
-
- 
+   
+  finalScore = `Player Score: ${playerScore(playScore)} : AI Score: ${aiScore(cScore)}`;
+  document.getElementById("test1").innerHTML = finalScore;
+}
